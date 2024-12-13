c7adae5e492d89eedf994407088e7a2d50e47baf
Rakesh1002 <rakeshroushan@Live.com>
2024-12-13 22:11:43 +0530

    remove edge runtime

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -2,9 +2,6 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import DemoWrapper from "../components/DemoWrapper";
 
-export const runtime = "edge";
-export const preferredRegion = "sin1";
-
 export default async function DemoPage() {
   const session = await auth();
 
